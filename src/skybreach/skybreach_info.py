4a0b8878a7e4e33906de8754e511f019cce19028
Roman Dovgan <romadovg@gmail.com>
2022-07-22 17:53:10 +0300

    #7 Implemented method to get Land info by coordinates

--- src/skybreach/skybreach_info.py
+++ src/skybreach/skybreach_info.py
@@ -1,6 +1,7 @@
 from web3 import Web3
 
 import resources.config
+import coordinates
 import rmrk_token
 
 providerRpc = {
@@ -8,6 +9,8 @@ providerRpc = {
     "alphanet": "https://rpc.api.moonbase.moonbeam.network",
 }
 web3 = Web3(Web3.HTTPProvider(providerRpc["development"]))  # Change to correct network
+
+contract_skybreach = resources.config.CONTRACT_ADDRESS_SKYBREACH
 
 with open('../abi/skybreach-abi.json', 'r') as file:
     abi_skybreach = file.read().replace('\n', '')
@@ -23,13 +26,21 @@ def print_balance():
     print(f"The balance of {address_moon} is: {balance_moon_rmrk} {rmrk_token.get_symbol()}")
 
 
-print_balance()
+def call_contract():
+    print(f'Making a call to contract at address: {contract_skybreach}')
+    contract_skybreach_component = web3.eth.contract(address=Web3.toChecksumAddress(contract_skybreach), abi=abi_skybreach)
+    dutch_duration = contract_skybreach_component.functions.getCreatorFee().call()
+    print(f'Dutch Duration: {dutch_duration} ')
 
-contract_skybreach = resources.config.CONTRACT_ADDRESS_SKYBREACH
 
-print(f'Making a call to contract at address: {contract_skybreach}')
+def get_land_info(land_id: int):
+    print(f'Making a call to get land info: {coordinates.convert_to_coordinates(land_id)}')
+    contract_skybreach_component = web3.eth.contract(address=Web3.toChecksumAddress(contract_skybreach), abi=abi_skybreach)
+    land_info = contract_skybreach_component.functions.getPlotData(land_id).call()
+    print(f'Land info: {land_info} ')
 
-contract_skybreach_component = web3.eth.contract(address=Web3.toChecksumAddress(contract_skybreach), abi=abi_skybreach)
-dutch_duration = contract_skybreach_component.functions.getCreatorFee().call()
 
-print(f'Dutch Duration: {dutch_duration} ')
+get_land_info(coordinates.convert_to_id(143, 133))
+get_land_info(coordinates.convert_to_id(144, 133))
+get_land_info(coordinates.convert_to_id(145, 133))
+
