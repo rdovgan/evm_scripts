214d14a77aea07f7caf9112529905d254660a4f4
Roman Dovgan <romadovg@gmail.com>
2022-07-25 16:00:15 +0300

    #15 Implemented job to update land_to_owner records

--- src/skybreach/db_connection.py
+++ src/skybreach/db_connection.py
@@ -1,12 +1,6 @@
 import mysql.connector
 from configparser import ConfigParser
 
-'''
-records_to_insert = [(4, 'HP Pavilion Power', 1999, '2019-01-11'),
-                         (5, 'MSI WS75 9TL-496', 5799, '2019-02-27'),
-                         (6, 'Microsoft Surface', 2330, '2019-07-23')]
-'''
-
 
 def read_db_config(filename='../../resources/config.ini', section='mysql'):
     """ Read database configuration file and return a dictionary object
@@ -26,20 +20,15 @@ def read_db_config(filename='../../resources/config.ini', section='mysql'):
             db[item[0]] = item[1]
     else:
         raise Exception('{0} not found in the {1} file'.format(section, filename))
-
     return db
 
 
-def insert_records(records_to_insert):
+def insert_land_records(records_to_insert):
     connection = None
     cursor = None
     try:
         db_configuration = read_db_config()
-        connection = mysql.connector.connect(host=db_configuration['host'],
-                                             database=db_configuration['database'],
-                                             user=db_configuration['user'],
-                                             password=db_configuration['password'],
-                                             port=db_configuration['port'])
+        connection = mysql.connector.connect(**db_configuration)
 
         insert_query = """INSERT INTO land_info (id, coord_x, coord_y, cyber, steampunk, wind, volcano, fire, water, 
                 necro, mecha, dragon, meadow, is_shore, is_island, is_mountain_foot, rarity, entropy)
@@ -48,7 +37,7 @@ def insert_records(records_to_insert):
         cursor = connection.cursor()
         cursor.executemany(insert_query, records_to_insert)
         connection.commit()
-        print(cursor.rowcount, "Record inserted successfully into Laptop table")
+        print(cursor.rowcount, "Record inserted successfully into skybreach.land_info table")
 
     except mysql.connector.Error as error:
         print("Failed to insert record into MySQL table {}".format(error))
@@ -85,4 +74,52 @@ def read_all():
             print("MySQL connection is closed")
 
 
-#read_all()
+def insert_land_to_owner_records(records_to_insert):
+    connection = None
+    cursor = None
+    try:
+        db_configuration = read_db_config()
+        connection = mysql.connector.connect(**db_configuration)
+
+        insert_query = """INSERT INTO land_to_owner (land_id, owner, price) VALUES (%s, %s, %s) ON DUPLICATE KEY UPDATE update_date = CURRENT_TIMESTAMP()"""
+
+        cursor = connection.cursor()
+        cursor.executemany(insert_query, records_to_insert)
+        connection.commit()
+        print(cursor.rowcount, "Record inserted successfully into skybreach.land_to_owner table")
+
+    except mysql.connector.Error as error:
+        print("Failed to insert record into MySQL table {}".format(error))
+
+    finally:
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+            print("MySQL connection is closed")
+
+
+def get_lands_to_owners():
+    connection = None
+    cursor = None
+    try:
+        db_configuration = read_db_config()
+        connection = mysql.connector.connect(**db_configuration)
+
+        sql_select_query = "select * from land_to_owner"
+        cursor = connection.cursor()
+        cursor.execute(sql_select_query)
+
+        records = cursor.fetchall()
+        print("Total number of rows in table: ", cursor.rowcount)
+
+        return records
+
+    except mysql.connector.Error as e:
+        print("Error reading data from MySQL table", e)
+    finally:
+        if connection.is_connected():
+            connection.close()
+            cursor.close()
+            print("MySQL connection is closed")
+
+
--- src/skybreach/land_info_job.py
+++ src/skybreach/land_info_job.py
@@ -1,5 +1,6 @@
 from web3 import Web3
 
+import numpy as np
 import resources.variables
 import coordinates
 import db_connection
@@ -16,14 +17,15 @@ contract_skybreach = resources.variables.CONTRACT_ADDRESS_SKYBREACH
 with open('../abi/skybreach-abi.json', 'r') as file:
     abi_skybreach = file.read().replace('\n', '')
 
+contract_skybreach_component = web3.eth.contract(address=Web3.toChecksumAddress(contract_skybreach), abi=abi_skybreach)
+
 
 def get_land_info(land_id: int):
-    contract_skybreach_component = web3.eth.contract(address=Web3.toChecksumAddress(contract_skybreach), abi=abi_skybreach)
     land_info = contract_skybreach_component.functions.getPlotData(land_id).call()
     return land_info
 
 
-def process_job():
+def process_land_import_job():
     land_ids_from_db = [land_from_db[0] for land_from_db in db_connection.read_all()]
     for x in range(50, 256):
         lands_to_insert = []
@@ -40,7 +42,31 @@ def process_job():
                 print(error)
         if len(lands_to_insert) > 0:
             print(f'Lands retrieved for {x} column: {len(lands_to_insert)}')
-            db_connection.insert_records(lands_to_insert)
+            db_connection.insert_land_records(lands_to_insert)
 
 
-process_job()
+def split_array(array, size: int):
+    chunks = []
+    for i in range(0, len(array), size):
+        chunks.append(array[i:i+size])
+    return chunks
+
+
+def define_land_to_owner_list(lands, owners):
+    land_to_owner_list = []
+    size = min(len(lands), len(owners))
+    for i in range(0, size):
+        land_to_owner_list.append((lands[i], owners[i], None))
+    return land_to_owner_list
+
+
+def process_land_to_owner_import_job():
+    land_ids_from_db = [land_from_db[0] for land_from_db in db_connection.read_all()]
+    splits = split_array(land_ids_from_db, 250)
+    for land_split in splits:
+        owner_list = contract_skybreach_component.functions.getPlotOwners(land_split).call()
+        land_to_owner_list = define_land_to_owner_list(land_split, owner_list)
+        db_connection.insert_land_to_owner_records(land_to_owner_list)
+
+
+process_land_to_owner_import_job()
