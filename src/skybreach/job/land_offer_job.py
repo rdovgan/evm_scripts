bc0c6f5869f9823d994bb6b0c7f95d3e085c133c
Roman Dovgan <romadovg@gmail.com>
2022-07-31 12:46:26 +0300

    #39 Fixed DB mapping for land_to_owner

--- src/skybreach/job/land_offer_job.py
+++ src/skybreach/job/land_offer_job.py
@@ -8,9 +8,9 @@ def process_job_to_make_offers():
     land_to_owners_to_update = []
     try:
         for land_to_owner in land_to_owners_filtered_land_types:
-            price = service.define_land_price(land_to_owner[1])
+            price = service.define_land_price(land_to_owner[0])
             land_to_owners_to_update.append((land_to_owner[0], land_to_owner[1], price))
-            service.make_buy_offer(land_to_owner[1])
+            service.make_buy_offer(land_to_owner[0])
     finally:
         print(len(land_to_owners_to_update))
         db.insert_all_land_to_owner(land_to_owners_to_update)
