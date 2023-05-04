e8f775f80e352b46b36d03b8b4a4f37dcfea0b6b
Roman Dovgan <romadovg@gmail.com>
2023-05-04 15:00:25 +0300

    Configured read balance from BASE network

--- src/base.py
+++ src/base.py
@@ -3,16 +3,15 @@ from web3 import Web3
 import resources.variables
 
 provider_rpc = {
-    "development": "https://rpc.api.moonriver.moonbeam.network/",
-    "alphanet": "https://rpc.api.moonbase.moonbeam.network",
+    "testnet": "https://base-goerli.public.blastapi.io",
 }
-web3 = Web3(Web3.HTTPProvider(provider_rpc["development"]))  # Change to correct network
+web3 = Web3(Web3.HTTPProvider(provider_rpc["testnet"]))  # Change to correct network
 
 address_moon = resources.variables.ADDRESS_MOON
 address_main = resources.variables.ADDRESS_FIGA
 
-balance_moon = web3.fromWei(web3.eth.getBalance(address_moon), "ether")
-balance_main = web3.fromWei(web3.eth.getBalance(address_main), "ether")
+balance_moon = web3.from_wei(web3.eth.get_balance(address_moon), "ether")
+balance_main = web3.from_wei(web3.eth.get_balance(address_main), "ether")
 
-print(f"The balance of {address_moon} is: {balance_moon} MOVR")
-print(f"The balance of {address_main} is: {balance_main} MOVR")
+print(f"The balance of {address_moon} is: {balance_moon} ETH")
+print(f"The balance of {address_main} is: {balance_main} ETH")
