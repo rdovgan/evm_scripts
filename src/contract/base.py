6d4cc1c2ea84e01376c0df3e978acb002bc49b63
Roman Dovgan <romadovg@gmail.com>
2023-05-05 13:21:54 +0300

    Refactored services to deploy and call smart contract methods

--- src/contract/base.py
+++ src/contract/base.py
@@ -1,8 +1,7 @@
-import json
 from web3 import Web3
-from solcx import compile_standard, install_solc
 
 import resources.variables
+import src.contract.contract_service as service
 
 provider_rpc = {
     "testnet": "https://base-goerli.public.blastapi.io",
@@ -12,62 +11,9 @@ web3 = Web3(Web3.HTTPProvider(provider_rpc["testnet"]))
 wallet_address = resources.variables.ADDRESS_GINGER
 private_key = resources.variables.PRIVATE_KEY_GINGER
 
+contract_name = "GoldOwner"
 
-def define_contract():
-    contract_name = "GoldOwner"
-    contract_filename = "GoldOwner.sol"
-    balance_ginger = web3.from_wei(web3.eth.get_balance(wallet_address), "ether")
-    print(f"The balance of {wallet_address} is: {balance_ginger} ETH")
-    install_solc('0.8.0')
-    with open(contract_filename, "r") as file:
-        gold_owner_file = file.read()
-    compiled_sol = compile_standard(
-        {
-            "language": "Solidity",
-            "sources": {contract_filename: {"content": gold_owner_file}},
-            "settings": {
-                "outputSelection": {
-                    "*": {
-                        "*": ["abi", "metadata", "evm.bytecode", "evm.bytecode.sourceMap"]
-                    }
-                }
-            },
-        },
-        solc_version="0.8.0",
-    )
-    compiled_contract_file = wallet_address[0:5] + "()" + wallet_address[-3:] + "-" + "compiled_code.json"
-    print(compiled_sol)
-    with open(compiled_contract_file, "w") as file:
-        json.dump(compiled_sol, file)
-    # get bytecode
-    bytecode = compiled_sol["contracts"][contract_filename][contract_name]["evm"]["bytecode"]["object"]
-    # get abi
-    abi = json.loads(compiled_sol["contracts"][contract_filename][contract_name]["metadata"])["output"]["abi"]
-    # Create the contract in Python
-    return web3.eth.contract(abi=abi, bytecode=bytecode)
+GoldOwnerContract = service.define_contract(web3, wallet_address, contract_name)
+contract_address = service.deploy_contract(GoldOwnerContract, web3, wallet_address, private_key, contract_name)
 
-
-GoldOwnerContract = define_contract()
-# Get the number of the latest transaction
-nonce = web3.eth.get_transaction_count(wallet_address)
-
-# build transaction
-transaction = GoldOwnerContract.constructor().build_transaction(
-    {
-        "gasPrice": web3.eth.gas_price,
-        "from": wallet_address,
-        "nonce": nonce,
-    }
-)
-# Sign the transaction
-sign_transaction = web3.eth.account.sign_transaction(transaction, private_key=private_key)
-
-# Send the transaction
-print("Deploying Contract!")
-transaction_hash = web3.eth.send_raw_transaction(sign_transaction.rawTransaction)
-
-# Wait for the transaction to be mined, and get the transaction receipt
-print("Waiting for transaction to finish...")
-transaction_receipt = web3.eth.wait_for_transaction_receipt(transaction_hash)
-print(f"Done! Contract deployed to {transaction_receipt.contractAddress}")
-
+service.call_make_gold(GoldOwnerContract, web3, wallet_address, private_key, contract_address)
---
+++ src/contract/contract_service.py
@@ -0,0 +1,90 @@
+import json
+import os
+
+from hexbytes import HexBytes
+from solcx import compile_standard, install_solc
+
+import src.contract.db_connection as db
+
+
+def define_contract(web3, wallet_address, contract_name):
+    compiled_contract_file = wallet_address[2:7] + ":" + contract_name + ".json"
+    contract_filename = contract_name + ".sol"
+
+    if os.path.isfile(compiled_contract_file):
+        with open(compiled_contract_file, "r") as file:
+            compiled_sol = json.load(file)
+    else:
+        compiled_sol = generate_contract_compilation(web3, wallet_address, contract_name)
+        with open(compiled_contract_file, "w") as file:
+            json.dump(compiled_sol, file)
+    # get bytecode
+    bytecode = compiled_sol["contracts"][contract_filename][contract_name]["evm"]["bytecode"]["object"]
+    # get abi
+    abi = json.loads(compiled_sol["contracts"][contract_filename][contract_name]["metadata"])["output"]["abi"]
+    # Create the contract in Python
+    return web3.eth.contract(abi=abi, bytecode=bytecode)
+
+
+def generate_contract_compilation(web3, wallet_address, contract_name):
+    contract_filename = contract_name + ".sol"
+    balance_ginger = web3.from_wei(web3.eth.get_balance(wallet_address), "ether")
+    print(f"The balance of {wallet_address} is: {balance_ginger} ETH")
+    install_solc('0.8.0')
+    with open(contract_filename, "r") as file:
+        gold_owner_file = file.read()
+    compiled_sol = compile_standard(
+        {
+            "language": "Solidity",
+            "sources": {contract_filename: {"content": gold_owner_file}},
+            "settings": {
+                "outputSelection": {
+                    "*": {
+                        "*": ["abi", "metadata", "evm.bytecode", "evm.bytecode.sourceMap"]
+                    }
+                }
+            },
+        },
+        solc_version="0.8.0",
+    )
+    return compiled_sol
+
+
+def deploy_contract(contract_object, web3, wallet_address, private_key, contract_name):
+    # check if contract was deployed before
+    contract_data = db.read_contract(wallet_address)
+    if contract_data is not None:
+        return contract_data[2]
+
+    # build transaction
+    transaction = contract_object.constructor().build_transaction(
+        {
+            "gasPrice": web3.eth.gas_price,
+            "from": wallet_address,
+            "nonce": web3.eth.get_transaction_count(wallet_address),
+        }
+    )
+    # Sign the transaction
+    sign_transaction = web3.eth.account.sign_transaction(transaction, private_key=private_key)
+    # Send the transaction
+    print("Deploying Contract!")
+    transaction_hash = web3.eth.send_raw_transaction(sign_transaction.rawTransaction)
+    # Wait for the transaction to be mined, and get the transaction receipt
+    print("Waiting for transaction to finish...")
+    transaction_contract_address = web3.eth.wait_for_transaction_receipt(transaction_hash).contractAddress
+    print(f"Done! Contract deployed to {transaction_contract_address}")
+    db.insert_contract((wallet_address, contract_name, transaction_contract_address))
+    return db.read_contract(wallet_address)[2]
+
+
+def call_make_gold(contract_object, web3, wallet_address, private_key, contract_address):
+    make_gold = contract_object.functions.makeGold().build_transaction(
+        {"from": wallet_address, "to": contract_address, "nonce": web3.eth.get_transaction_count(wallet_address)})
+    # Sign the transaction
+    sign_store_contact = web3.eth.account.sign_transaction(
+        make_gold, private_key=private_key
+    )
+    # Send the transaction
+    send_store_contact = web3.eth.send_raw_transaction(sign_store_contact.rawTransaction)
+    transaction_receipt = web3.eth.wait_for_transaction_receipt(send_store_contact)
+    print(f"Submitted contract method execution {HexBytes.hex(transaction_receipt.transactionHash)}")
