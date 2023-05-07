1c3be87644ee484750b6786ffaf9a76dfb2d69c0
Roman Dovgan <romadovg@gmail.com>
2023-05-07 13:13:01 +0300

    Implemented transaction delay and gas price check for arbitrum_nova

--- src/contract/arbitrum_nova.py
+++ src/contract/arbitrum_nova.py
@@ -9,13 +9,16 @@ from web3 import Web3
 from wallet import wallets as w
 
 # wait up to 20 minutes
-# delay = random.randint(1, 20) * 60
-# sleep(delay)
+delay = random.randint(1, 20) * 60
+sleep(delay)
 
 provider_rpc = {
     "mainnet": "https://arbitrum-nova.public.blastapi.io",
 }
 web3 = Web3(Web3.HTTPProvider(provider_rpc["mainnet"]))
+
+if web3.eth.gas_price > 100000000:
+    raise "Gas price is too high"
 
 addresses = [(w.ADDRESS_BANANA, w.PRIVATE_KEY_BANANA), (w.ADDRESS_MANGO, w.PRIVATE_KEY_MANGO), (w.ADDRESS_GUAVA, w.PRIVATE_KEY_GUAVA),
              (w.ADDRESS_GINGER, w.PRIVATE_KEY_GINGER),
