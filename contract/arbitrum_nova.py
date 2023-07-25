57cdcfd7c3187c02a40668d9ab800a9b5da5a7be
Roman Dovgan <romadovg@gmail.com>
2023-07-25 15:46:13 +0300

    Fixed RPC name for Arbitrum Nova

--- contract/arbitrum_nova.py
+++ contract/arbitrum_nova.py
@@ -14,10 +14,10 @@ log_name = 'arb_nova.log'
 
 # wait from up to 20 minutes
 delay = random.randint(1, 20) * random.randint(5, 30) * random.randint(1, 2)
-sleep(delay)
+# sleep(delay)
 print(f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")} Started Arbitrum Nova job')
 
-web3 = Web3(Web3.HTTPProvider(rpc.provider['arbitrum_nova']))
+web3 = Web3(Web3.HTTPProvider(rpc.provider['arb_nova']))
 
 if web3.eth.gas_price > 100000000:
     raise "Gas price is too high"
@@ -42,5 +42,5 @@ times = random.randint(0, 4) + random.randint(0, 4)
 
 log(log_name, f"Prepare to make {times} transactions")
 for x in range(times):
-    sleep(random.randint(2, 7) * random.randint(7, 11) - random.randint(13, 19))
+    # sleep(random.randint(2, 7) * random.randint(7, 11) - random.randint(13, 19))
     service.call_make_gold(gold_counter_contract, web3, wallet_address, private_key, contract_address, log_name)
