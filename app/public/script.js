0f7f529c1c34f3e727566e68daecca93421c0fa1
Roman Dovgan <romadovg@gmail.com>
2023-08-15 16:00:25 +0300

    Added logs

--- app/public/script.js
+++ app/public/script.js
@@ -1,5 +1,12 @@
 // Initialize Web3
-const web3 = new Web3(window.ethereum);
+let web3;
+
+// Check if MetaMask is installed
+if (typeof window.ethereum !== 'undefined') {
+    web3 = new Web3(window.ethereum);
+} else {
+    console.log("MetaMask not found. Please install MetaMask and try again.");
+}
 
 // Contract ABI and Address
 const contractABI = [{"inputs":[],"name":"retrieve","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"num","type":"uint256"}],"name":"store","outputs":[],"stateMutability":"nonpayable","type":"function"}];
@@ -11,10 +18,13 @@ const contract = new web3.eth.Contract(contractABI, contractAddress);
 // Function to call a contract method
 async function callContractMethod() {
     try {
-        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-        const account = accounts[0];
-
         const inputValue = document.getElementById('inputValue').value;
+        console.log('Calling store() with value:', inputValue);
+
+        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+        console.log('Got accounts:', accounts);
+        const account = accounts[0];
+        console.log('Using account:', account);
 
         const result = await contract.methods.store(inputValue).call({ from: account });
         console.log('Method Result:', result);
